3602dce61f9635e4dbdb3afc193ff4892960a3de
Volodymyr Vinyarskyy <v.winner@gmail.com>
2017-09-08 19:43:23 +0300

    Initial Commit

---
+++ FinanceMan/RCButton.swift
@@ -0,0 +1,25 @@
+//
+//  RCButton.swift
+//  FinanceMan
+//
+//  Created by Володимир on 9/8/17.
+//  Copyright © 2017 Володимир. All rights reserved.
+//
+
+import UIKit
+
+class RCButton: UIButton {
+    required init() {
+        super.init(frame: .zero)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.height / 3
+        clipsToBounds = true
+    }
+
+}
---
+++ FinanceMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinanceMan
+//
+//  Created by Володимир on 9/8/17.
+//  Copyright © 2017 Володимир. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
