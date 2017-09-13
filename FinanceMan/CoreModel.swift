50e248d4e715d8adc7ac491dfd950662dd822546
ITA student <student@ITAs-Mac-mini.local>
2017-09-13 16:38:06 +0300

    add code/decode collections

--- FinanceMan/CoreModel.swift
+++ FinanceMan/CoreModel.swift
@@ -10,18 +10,50 @@ import Foundation
 
 class CoreModel {
     
-    var categories = [CategoryModel]()
-    var transactions = [TransactionModel]()
+    private var categories = [CategoryModel]()
+    private var transactions = [TransactionModel]()
     
     func addCategory(id: UInt, name: String){
         let newCategory = CategoryModel(ID: id, CategoryName: name)
         categories.append(newCategory)
     }
     
-    func addTransaction(id: UInt, category: CategoryModel, bill: String){
-        let newTransaction = TransactionModel(category: <#T##CategoryModel#>, bill: <#T##String#>)
+    func getCategory(name:String) -> CategoryModel?{
+        for category:CategoryModel in categories{
+            if category.getName() == name {
+                return category
+            }
+        }
+        return nil
+    }
+    
+    
+    
+    func addTransaction(category: CategoryModel, bill: String, date: String){
+        let newTransaction = TransactionModel(category: category, bill: bill, date: date)
         transactions.append(newTransaction)
     }
+    
+    func saveTransactions(){
+        let encodedData = NSKeyedArchiver.archivedData(withRootObject: transactions)
+        UserDefaults.standard.set(encodedData, forKey: "transactions")
+        
+    }
+    
+    func retrievTranactions(){
+        if let data = UserDefaults.standard.data(forKey: "transactions"),
+            let transactionList = NSKeyedUnarchiver.unarchiveObject(with: data) as? [TransactionModel] {
+            transactions = transactionList
+        }
+    }
+    
+    func retrievCategories(){
+        if let data = UserDefaults.standard.data(forKey: "categories"),
+            let categireList = NSKeyedUnarchiver.unarchiveObject(with: data) as? [CategoryModel] {
+            categories = categireList
+        }
+    }
+    
     
     
     
--- FinanceMan/TransactionModel.swift
+++ FinanceMan/TransactionModel.swift
@@ -13,19 +13,25 @@ class TransactionModel: NSObject, NSCoding {
     private var _id: String = ""
     private var _category: CategoryModel?
     private var _bill: NSDecimalNumber?
-    private var description: String?
+    private var descriptionTransaction: String?
+    private var _date: Date?
+    
     
     //magic numbers :)
     //Only two numbers after a point are taken into account
     //two last digits in _bill for bit rate
     private let bitRate = 100
+    private let dateFormatter = DateFormatter()
     
-    init(category: CategoryModel, bill: String) {
+    init(category: CategoryModel, bill: String, date: String) {
         self._id = UUID().uuidString
         self._category = category
-        self.billStr = bill
+        self._bill = NSDecimalNumber(string: bill)
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        self._date = dateFormatter.date(from: date)
     }
-  
+    
+    //NSCoder import
     required init(coder decoder: NSCoder) {
         if let idDecode = decoder.decodeObject(forKey: "id") as? String {
             _id = idDecode
@@ -33,27 +39,39 @@ class TransactionModel: NSObject, NSCoding {
         if let categoryDecode = decoder.decodeObject(forKey: "category") as? CategoryModel {
             _category = categoryDecode
         }
-        if let billDecode = decoder.decodeObject(forKey: "bill") as? Int {
+        if let billDecode = decoder.decodeObject(forKey: "bill") as? NSDecimalNumber {
             _bill = billDecode
         }
-    }
-    
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(id, forKey: "id")
-        aCoder.encode(category, forKey: "category")
-        aCoder.encode(bill, forKey: "bill")
-    }
-    
-    private var id: String {
-        set {
-            if _id == "" {
-               _id = newValue
-            }
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        if let dateDecode = decoder.decodeObject(forKey: "date") as? Date {
+            _date = dateDecode
         }
+    }
+    
+    //NSCoder export
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(_id, forKey: "id")
+        aCoder.encode(_category, forKey: "category")
+        aCoder.encode(_bill, forKey: "bill")
+        aCoder.encode(_date, forKey: "date")
+    }
+    
+    //Getters and Setters
+    //id:String
+    private var id: String {
         get {
             return _id
         }
+        set {
+            if _id == "" {
+                _id = newValue
+            } else {
+                _id = UUID().uuidString
+            }
+        }
     }
+    
+    //category:Category
     private var category: CategoryModel {
         get {
             return _category!
@@ -63,52 +81,32 @@ class TransactionModel: NSObject, NSCoding {
         }
     }
     
-    
-    private var billStr: String {
-        get {
-            return int2str(_bill)
-        }
-        set {
-            str2int(newValue)
-        }
-    }
-    
     private var bill: Double {
         get{
-            return int2double(_bill)
+            return (_bill?.doubleValue)!
         }
         set {
-            _bill = double2int(newValue)
+            _bill = NSDecimalNumber(value: newValue)
         }
     }
     
-    //convert from Internal Integer format to Double
-    func int2double(int: Int) -> Double{
-        return Double(int / bitRate) + (Double(Double(int % bitRate) / Double(bitRate)))
-    }
-    
-    //convert from Double to Internal Integer format
-    //Only two numbers after a point are taken into account
-    func double2int(double: Double) -> Int{
-        return Int(double * bitRate)
-    }
-    
-    //convert from String to the Internal Integer format
-    func str2int(str: String) -> Int{
-        
-        if let _ = Double(str) {
-            let billPars = str.components(separatedBy: ".")
-            return (Int(billPars[0])!) * bitRate + Int(billPars[1])!
+    private var billStr: String {
+        get{
+            return (_bill?.stringValue)!
+        }
+        set{
+            _bill = NSDecimalNumber(string: newValue)
         }
     }
-    //convert from internal Integer to String
-    func int2str(int: Int) -> String{
-        
-        if (int % bitRate) > 1 {
-            let integerPart = int / bitRate
-            let floatPart = int - integerPart
-            return "\(integerPart).\(floatPart)"
+    private var date: String {
+        get{
+            return dateFormatter.string(from:_date!)
+
         }
-        return "\(int / bitRate)"
+        set{
+            _date = dateFormatter.date(from:newValue)
+            
+        }
     }
+    
 }
