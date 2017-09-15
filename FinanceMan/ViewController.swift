1e2c654bfb4f432d53f6217f10db6abb42372b49
ITA student <student@ITAs-Mac-mini.local>
2017-09-15 14:19:56 +0300

    customize NavigationBar

--- FinanceMan/ViewController.swift
+++ FinanceMan/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate {
 
+    static private var coreModel = CoreModel()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: 320, height: 44))
-        self.view.addSubview(navBar);
-        let navItem = UINavigationItem(title: "Transactions");
-        let doneItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: nil, action: #selector(getter: UIAccessibilityCustomAction.selector));
-        navItem.rightBarButtonItem = doneItem;
-        navBar.setItems([navItem], animated: false);
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
